becf19638d8d38df7106240634b3296908c00b98
Muklah Hilal <M-M.mexat@hotmail.com>
2018-12-10 23:59:25 +0300

    first commit

---
+++ app/src/main/java/com/example/muklahhn/buildasinglescreenapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.muklahhn.buildasinglescreenapp;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CollapsingToolbarLayout collapsingToolbarLayout = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(false);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(" ");
+
+        toolbarText();
+    }
+
+    private void toolbarText() {
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.collapsedbar);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.expandedbar);
+
+    }
+}
